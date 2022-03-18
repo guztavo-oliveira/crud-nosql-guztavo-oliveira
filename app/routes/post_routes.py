092936814f92752c58121bbedac18bf1c9cd6f09
Gustavo <gu.machado.oliveira@gmail>
2022-03-18 17:20:38 -0300

    refactor: changed the routes from "/post" to "/posts"

--- app/routes/post_routes.py
+++ app/routes/post_routes.py
@@ -7,24 +7,24 @@ def post_routes(app):
     def home():
         return "ok"
 
-    @app.post("/post")
+    @app.post("/posts")
     def new_post():
 
         return post_controller.add_post()
 
-    @app.get("/post")
+    @app.get("/posts")
     def get_all():
 
         return post_controller.get_all()
 
-    @app.get("/post/<int:id>")
+    @app.get("/posts/<int:id>")
     def get_post_by_id(id: int):
         return post_controller.get_by_id(id)
 
-    @app.patch("/post/<int:id>")
+    @app.patch("/posts/<int:id>")
     def update_post(id: int):
         return post_controller.update_post(id)
 
-    @app.delete("/post/<int:id>")
+    @app.delete("/posts/<int:id>")
     def delete_post(id: id):
         return post_controller.delete_post(id)
